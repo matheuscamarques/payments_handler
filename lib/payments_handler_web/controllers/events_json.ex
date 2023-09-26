33b62775a11a1ba5037cd017cc96c4fc4f129958
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-26 13:56:14 -0300

    add: Create Events Json to prepare a data set

---
+++ lib/payments_handler_web/controllers/events_json.ex
@@ -0,0 +1,42 @@
+defmodule PaymentsHandlerWeb.EventsJSON do
+  alias PaymentsHandler.Payments.Events
+  alias PaymentsHandler.Payments
+
+  def withdraw_json(%Events{
+        origin: origin
+      }) do
+    %{
+      origin: %{
+        id: origin,
+        balance: Payments.get_balance_by_id(origin)
+      }
+    }
+  end
+
+  def deposit_json(%Events{
+        destination: destination
+      }) do
+    %{
+      destination: %{
+        id: destination,
+        balance: Payments.get_balance_by_id(destination)
+      }
+    }
+  end
+
+  def transfer_json(%Events{
+        origin: origin,
+        destination: destination
+      }) do
+    %{
+      origin: %{
+        id: origin,
+        balance: Payments.get_balance_by_id(origin)
+      },
+      destination: %{
+        id: destination,
+        balance: Payments.get_balance_by_id(destination)
+      }
+    }
+  end
+end
