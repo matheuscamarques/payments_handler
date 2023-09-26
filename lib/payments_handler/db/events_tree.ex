d2062c554704fedbc339b70edf4b826f12540ab9
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-26 13:51:41 -0300

    add: Define a TreeBehavior for the database engine

---
+++ lib/payments_handler/db/events_tree.ex
@@ -0,0 +1,13 @@
+defmodule PaymentsHandler.Db.EventsServer.EventsTree do
+  @behaviour PaymentsHandler.Db.EventsServer.TreeBehaviour
+  alias PaymentsHandler.Db.EventsServer.TreeStrategy
+  alias PaymentsHandler.Payments.Events
+
+  defdelegate construct, to: TreeStrategy, as: :construct
+  defdelegate get_from_id(tree, index), to: TreeStrategy, as: :get_from_id
+
+  @spec insert(tree :: any, index :: any, item :: PaymentsHandler.Payments.Events) :: AVLTree
+  def insert(tree, index, %Events{} = item) do
+    TreeStrategy.insert(tree, index, item)
+  end
+end
