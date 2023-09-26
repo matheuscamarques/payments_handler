a5ba279e2417d835ec0376d460b57f8982bf7987
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-26 14:10:45 -0300

    chore: error json file

--- test/payments_handler_web/controllers/error_json_test.exs
+++ test/payments_handler_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule PaymentsHandlerWeb.ErrorJSONTest do
   use PaymentsHandlerWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert PaymentsHandlerWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert PaymentsHandlerWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
