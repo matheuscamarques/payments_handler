1dc72af4d2aca133881688b904b4b491bc71a725
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-26 13:55:28 -0300

    fix: Adding origin value to Events struct

--- lib/payments_handler/payments/events.ex
+++ lib/payments_handler/payments/events.ex
@@ -1,10 +1,11 @@
 defmodule PaymentsHandler.Payments.Events do
-  @enforce_keys [:amount, :destination, :type]
+  @enforce_keys [:amount, :destination, :type, :origin]
 
   @type t :: %__MODULE__{
           amount: integer(),
           destination: String.t(),
+          origin: String.t(),
           type: String.t()
         }
-  defstruct [:amount, :destination, :type]
+  defstruct [:amount, :destination, :type, :origin]
 end
