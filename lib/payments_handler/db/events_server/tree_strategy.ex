83f2cf47c0de44c8cf8f8604ea3f51f77af115c2
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-26 13:52:24 -0300

    add: Implement the TreeStrategy

---
+++ lib/payments_handler/db/events_server/tree_strategy.ex
@@ -0,0 +1,19 @@
+defmodule PaymentsHandler.Db.EventsServer.TreeStrategy do
+  alias AVLTree
+  @behaviour PaymentsHandler.Db.EventsServer.TreeBehaviour
+
+  def construct(), do: AVLTree.new(fn {index_a, _}, {index_b, _} -> index_a < index_b end)
+
+  def get_from_id(tree, index), do: AVLTree.get(tree, {index, nil})
+
+  def insert(tree, index, item) do
+    case get_from_id(tree, index) do
+      nil ->
+        AVLTree.put(tree, {index, [item]})
+
+      {_index, values} ->
+        acc = values ++ [item]
+        AVLTree.put(tree, {index, acc})
+    end
+  end
+end
