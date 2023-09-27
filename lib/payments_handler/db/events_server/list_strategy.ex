062878b1e17111ea632be5b2eab13bf978e46e54
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-27 16:39:04 -0300

    add: list strategy implementation

---
+++ lib/payments_handler/db/events_server/list_strategy.ex
@@ -0,0 +1,20 @@
+defmodule PaymentsHandler.Db.EventsServer.ListStrategy do
+  @behaviour PaymentsHandler.Db.Strategy.DbEngineBehaviour
+
+  def construct(), do: []
+
+  def get_from_id(list, index) do
+    Enum.find(list, fn {idx, _values} -> index == idx end)
+  end
+
+  def insert(list, index, item) do
+    case get_from_id(list, index) do
+      nil ->
+        [{index, [item]} | list]
+
+      {_index, values} ->
+        position = Enum.find_index(list, fn {idx, _values} -> index == idx end)
+        List.keyreplace(list, index, position, {index, [item | values]})
+    end
+  end
+end
