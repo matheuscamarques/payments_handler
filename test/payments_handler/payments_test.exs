32d2af11dd97adf0c1d0811f50df0fe645913617
Matheus C Marques <matheuscamarques@gmail.com>
2023-09-30 12:42:48 -0300

    fix: return of fucntions

--- test/payments_handler/payments_test.exs
+++ test/payments_handler/payments_test.exs
@@ -11,42 +11,43 @@ defmodule PaymentsHandler.PaymentsTest do
   describe "Reset state" do
     test "before starting tests" do
       Payments.reset()
-      assert Payments.get_balance_by_id("SYSTEM_EVENT") == 100_000_000_000
+
+      assert Payments.get_balance_by_id("SYSTEM_EVENT") ==
+               {:ok, 100_000_000_000, %{balance: 100_000_000_000}}
     end
   end
 
   describe "Get balance" do
     test "for non-existing account" do
       Payments.reset()
-      balance_sys = Payments.get_balance_by_id("SYSTEM_EVENT")
+      {:ok, balance_sys, %{balance: balance_sys}} = Payments.get_balance_by_id("SYSTEM_EVENT")
       assert balance_sys == 100_000_000_000
 
-      balance = Payments.get_balance_by_id(100)
-      assert balance == 0
+      assert {:ok, 0, %{balance: 0}} = Payments.get_balance_by_id(100)
     end
 
     test "existing account" do
       Payments.reset()
 
-      assert Payments.transfer(%Events{
-               amount: 10,
-               origin: "SYSTEM_EVENT",
-               destination: 100,
-               type: "deposit"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 10,
+                 origin: "SYSTEM_EVENT",
+                 destination: 100,
+                 type: "deposit"
+               })
 
-      balance = Payments.get_balance_by_id(100)
-      assert balance == 10
+      assert {:ok, 10, _} = Payments.get_balance_by_id(100)
 
-      assert Payments.transfer(%Events{
-               amount: 10,
-               origin: "SYSTEM_EVENT",
-               destination: 100,
-               type: "deposit"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 10,
+                 origin: "SYSTEM_EVENT",
+                 destination: 100,
+                 type: "deposit"
+               })
 
-      balance = Payments.get_balance_by_id(100)
-      assert balance == 20
+      {:ok, 20, %{balance: 20}} = Payments.get_balance_by_id(100)
     end
   end
 
@@ -60,15 +61,15 @@ defmodule PaymentsHandler.PaymentsTest do
     test "with initial balance" do
       Payments.reset()
 
-      assert Payments.transfer(%Events{
-               amount: 10,
-               origin: "SYSTEM_EVENT",
-               destination: 100,
-               type: "deposit"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 10,
+                 origin: "SYSTEM_EVENT",
+                 destination: 100,
+                 type: "deposit"
+               })
 
-      balance = Payments.get_balance_by_id(100)
-      assert balance == 10
+      assert {:ok, 10, _} = Payments.get_balance_by_id(100)
     end
   end
 
@@ -76,25 +77,25 @@ defmodule PaymentsHandler.PaymentsTest do
     test "existing account" do
       Payments.reset()
 
-      assert Payments.transfer(%Events{
-               amount: 10,
-               origin: "SYSTEM_EVENT",
-               destination: 100,
-               type: "deposit"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 10,
+                 origin: "SYSTEM_EVENT",
+                 destination: 100,
+                 type: "deposit"
+               })
 
-      balance = Payments.get_balance_by_id(100)
-      assert balance == 10
+      assert {:ok, 10, %{balance: 10}} = Payments.get_balance_by_id(100)
 
-      assert Payments.transfer(%Events{
-               amount: 10,
-               origin: "SYSTEM_EVENT",
-               destination: 100,
-               type: "deposit"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 10,
+                 origin: "SYSTEM_EVENT",
+                 destination: 100,
+                 type: "deposit"
+               })
 
-      balance = Payments.get_balance_by_id(100)
-      assert balance == 20
+      assert {:ok, 20, %{balance: 20}} = Payments.get_balance_by_id(100)
     end
   end
 
@@ -113,29 +114,31 @@ defmodule PaymentsHandler.PaymentsTest do
     test "from existing account" do
       Payments.reset()
 
-      assert Payments.transfer(%Events{
-               amount: 10,
-               origin: "SYSTEM_EVENT",
-               destination: 100,
-               type: "deposit"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 10,
+                 origin: "SYSTEM_EVENT",
+                 destination: 100,
+                 type: "deposit"
+               })
 
-      assert Payments.transfer(%Events{
-               amount: 10,
-               origin: "SYSTEM_EVENT",
-               destination: 100,
-               type: "deposit"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 10,
+                 origin: "SYSTEM_EVENT",
+                 destination: 100,
+                 type: "deposit"
+               })
 
-      assert Payments.transfer(%Events{
-               amount: 5,
-               origin: 100,
-               destination: "SYSTEM_EVENT",
-               type: "withdraw"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 5,
+                 origin: 100,
+                 destination: "SYSTEM_EVENT",
+                 type: "withdraw"
+               })
 
-      balance = Payments.get_balance_by_id(100)
-      assert balance == 15
+      assert {:ok, 15, %{balance: 15}} = Payments.get_balance_by_id(100)
     end
   end
 
@@ -143,36 +146,40 @@ defmodule PaymentsHandler.PaymentsTest do
     test "from existing account" do
       Payments.reset()
 
-      assert Payments.transfer(%Events{
-               amount: 10,
-               origin: "SYSTEM_EVENT",
-               destination: 100,
-               type: "deposit"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 10,
+                 origin: "SYSTEM_EVENT",
+                 destination: 100,
+                 type: "deposit"
+               })
 
-      assert Payments.transfer(%Events{
-               amount: 10,
-               origin: "SYSTEM_EVENT",
-               destination: 100,
-               type: "deposit"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 10,
+                 origin: "SYSTEM_EVENT",
+                 destination: 100,
+                 type: "deposit"
+               })
 
-      assert Payments.transfer(%Events{
-               amount: 5,
-               origin: 100,
-               destination: "SYSTEM_EVENT",
-               type: "withdraw"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 5,
+                 origin: 100,
+                 destination: "SYSTEM_EVENT",
+                 type: "withdraw"
+               })
 
-      assert Payments.transfer(%Events{
-               amount: 15,
-               origin: 100,
-               destination: 300,
-               type: "withdraw"
-             }) == :ok
+      assert {:ok, _, _} =
+               Payments.transfer(%Events{
+                 amount: 15,
+                 origin: 100,
+                 destination: 300,
+                 type: "withdraw"
+               })
 
-      assert Payments.get_balance_by_id(100) == 0
-      assert Payments.get_balance_by_id(300) == 15
+      assert {:ok, 0, _} = Payments.get_balance_by_id(100)
+      assert {:ok, 15, _} = Payments.get_balance_by_id(300)
     end
 
     test "from from non-existing account" do
